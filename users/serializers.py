8c7d8d38733bd96b1a56978ad1ceceedbce563fc
Dimanskr <dimanskr2@gmail.com>
2024-11-19 22:51:31 +0300

    Add access control using JWT token

--- users/serializers.py
+++ users/serializers.py
@@ -12,14 +12,10 @@ class PaymentSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     payments = PaymentSerializer(many=True, read_only=True, required=False)
 
-    password = serializers.CharField(write_only=True)
-
     def create(self, validated_data):
-        # извлекаем данные о платежах и пароль
+        # извлекаем данные о платежах
         payments = validated_data.pop("payments", None)
-        password = validated_data.pop("password")
         user = User(**validated_data)
-        user.set_password(password)
         user.save()
 
         # Добаляем платежи
--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ from rest_framework import filters
 from rest_framework.generics import (CreateAPIView, DestroyAPIView,
                                      ListAPIView, RetrieveAPIView,
                                      UpdateAPIView)
+from rest_framework.permissions import AllowAny
 
 from users.models import Payment, User
 from users.serializers import PaymentSerializer, UserSerializer
@@ -10,6 +11,12 @@ from users.serializers import PaymentSerializer, UserSerializer
 
 class UserCreateAPIView(CreateAPIView):
     serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
+
+    def perform_create(self, serializer):
+        user = serializer.save(is_active=True)
+        user.set_password(user.password)
+        user.save()
 
 
 class UserListAPIView(ListAPIView):
@@ -25,6 +32,15 @@ class UserRetrieveAPIView(RetrieveAPIView):
 class UserUpdateAPIView(UpdateAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+
+    def perform_update(self, serializer):
+        # Сохраняем обновленные данные пользователя
+        user = serializer.save()
+
+        # Хэшируем пароль, если он передан
+        if 'password' in serializer.validated_data:
+            user.set_password(serializer.validated_data['password'])
+            user.save()
 
 
 class UserDestroyAPIView(DestroyAPIView):
