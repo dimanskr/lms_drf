1aa9559090ae55bca0f3df4cdb18018180c62e9c
Dimanskr <dimanskr2@gmail.com>
2024-11-26 15:27:26 +0300

    Add youtube validator and some fix to fixtures

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,22 +1,24 @@
+from rest_framework import serializers
 from rest_framework.fields import SerializerMethodField
-from rest_framework.serializers import ModelSerializer
 
 from materials.models import Course, Lesson
+from materials.validators import youtube_url_validator
 
 
-class CourseSerializer(ModelSerializer):
+class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
         fields = "__all__"
 
 
-class LessonSerializer(ModelSerializer):
+class LessonSerializer(serializers.ModelSerializer):
+    video_url = serializers.CharField(validators=[youtube_url_validator], required=False)
     class Meta:
         model = Lesson
         fields = "__all__"
 
 
-class CourseDetailSerializer(ModelSerializer):
+class CourseDetailSerializer(serializers.ModelSerializer):
 
     lessons_count = SerializerMethodField()
     lessons = LessonSerializer(many=True, read_only=True)
