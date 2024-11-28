e4186979f1742529d185c2c9f860388dbd7a846e
Dimanskr <dimanskr2@gmail.com>
2024-11-28 23:43:01 +0300

    Add settings to Bearer token authorization in swagger

--- config/settings.py
+++ config/settings.py
@@ -152,3 +152,13 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
+
+SWAGGER_SETTINGS = {
+    'SECURITY_DEFINITIONS': {
+        'api_key': {
+            'type': 'apiKey',
+            'in': 'header',
+            'name': 'Authorization'
+        }
+    },
+}
